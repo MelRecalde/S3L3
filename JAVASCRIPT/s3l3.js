ca5a8c682d61e0e11ae38aad4a386b02d727fcdb
MelRecalde <melquisedicrecalde@gmail.com>
2024-05-15 22:47:37 +0200

    fifth commit

--- JAVASCRIPT/s3l3.js
+++ JAVASCRIPT/s3l3.js
@@ -1,36 +1,47 @@
 const form = document.getElementById("addTask")
-// 
+// riferimento al form
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault()
-    const taskInputField = document.getElementById("task")
+    event.preventDefault() // evito l'aggiornamento al submit
 
+    const taskInputField = document.getElementById("task")
+    // seleziono l'elemento input per poi ricavare il valore
     const newTask = {
         task: taskInputField.value
     }
 
+    // creo un div e assegno una classe
     const card = document.createElement("div")
     card.classList.add("card")
 
-    const cardTask = document.createElement("p")
+    // creo un <span> e un <button>
+    const cardTask = document.createElement("span")
     const cardBtn = document.createElement("button")
-    cardTask.innerText = newTask.task
-    cardBtn.innerHTML = "DELETE"
-    cardBtn.classList.add("button")
+    cardTask.innerText = newTask.task // stampo quello che ho scritto
+    cardBtn.innerHTML = "<ion-icon name='trash-bin'></ion-icon>"
+    cardBtn.classList.add("button") // assegno la classe al button
     card.append(cardTask, cardBtn)
+    // Inserisco gli elementi all'interno della card
 
+    // creo un event attraverso una funzione per eliminare la card con il bottone
     cardBtn.addEventListener("click", function () {
         let allDeleteButtons = document.querySelectorAll('.button')
         for (let i = 0; i < allDeleteButtons.length; i++) {
             allDeleteButtons[i].addEventListener('click', function () {
-              this.parentNode.remove()
+                this.parentNode.remove()
             })
-          }
+        }
     })
-    
+
+    cardTask.addEventListener('click', function() {
+        allTask.classList.toggle("checked-task");
+    })
+
     const toDo = document.getElementById("listTask")
     toDo.appendChild(card)
+    // seleziono dove inserire le card create nella pagina
 
-    taskInputField.value = ""
+    taskInputField.value = "" // Ripulisco l'input
+
 }
 )
