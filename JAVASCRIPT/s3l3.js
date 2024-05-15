4460410c8e2928be5227ecd0176b011160d00462
MelRecalde <melquisedicrecalde@gmail.com>
2024-05-15 20:13:58 +0200

    forth commit

--- JAVASCRIPT/s3l3.js
+++ JAVASCRIPT/s3l3.js
@@ -13,14 +13,24 @@ form.addEventListener("submit", function (event) {
     card.classList.add("card")
 
     const cardTask = document.createElement("p")
+    const cardBtn = document.createElement("button")
     cardTask.innerText = newTask.task
-    card.append(cardTask)
+    cardBtn.innerHTML = "DELETE"
+    cardBtn.classList.add("button")
+    card.append(cardTask, cardBtn)
 
+    cardBtn.addEventListener("click", function () {
+        let allDeleteButtons = document.querySelectorAll('.button')
+        for (let i = 0; i < allDeleteButtons.length; i++) {
+            allDeleteButtons[i].addEventListener('click', function () {
+              this.parentNode.remove()
+            })
+          }
+    })
+    
     const toDo = document.getElementById("listTask")
     toDo.appendChild(card)
 
     taskInputField.value = ""
-
-    toDo
 }
 )
